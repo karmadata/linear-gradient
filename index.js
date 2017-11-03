da2e8e220cf7911d95b1f558f9eca04fd92bafa1
johnliu <john.liu@karmadata.com>
2017-11-03 11:19:11 -0400

    working and passed test

---
+++ index.js
@@ -0,0 +1,33 @@
+function Gradient(colors) {
+    // ensure constructor variables are valid
+    if (colors.length == null || colors.length < 2) throw new Error('needs at least two colors to instantiate linear-gradient');
+    for (var i = 0; i < colors.length; i++) {
+        var color = colors[i];
+        if (color.length == null || color.length !== 3) throw new Error('Error at element ' + i + '. Expected an RGB array such as [100, 100, 20], instead got ' + color);
+        for (var j = 0; j < 3; j ++) {
+            var c = color[j];
+            if (c < 0 || c > 255) throw new Error('Error at element ' + i + '. Expected an RGB array such as [100, 100, 20], instead got ' + color);
+        }
+    }
+
+    this.colors = colors;
+}
+
+Gradient.prototype.calcArray = function(value) {
+    if (value < 0 || value > 1) throw new Error('The input value must be between 0 and 1');
+    // offset represents where the value lies in the gradient
+    var offset = value * (this.colors.length - 1);
+    // whole is the whole number part, decimal is the remainder
+    var whole = Math.floor(offset);
+    var decimal = offset - whole;
+    // if no remainder, simply return the lower color, otherwise calculate
+    var lowerColor = this.colors[whole];
+    if (decimal === 0.0) return lowerColor;
+    var higherColor = this.colors[whole+1];
+    var r = lowerColor[0] * (1 - decimal) + higherColor[0] * decimal;
+    var g = lowerColor[1] * (1 - decimal) + higherColor[1] * decimal;
+    var b = lowerColor[2] * (1 - decimal) + higherColor[2] * decimal;
+    return [Math.round(r), Math.round(g), Math.round(b)];
+}
+
+module.exports = Gradient;
---
+++ test.js
@@ -0,0 +1,16 @@
+var Gradient = require('./index')
+
+var grad1 = new Gradient([
+    [0,0,0],
+    [0,80,0],
+    [0,160,80],
+    [80,80,80],
+    [160,40,40],
+    [255,0,0]
+]);
+
+console.log(grad1.calcArray(0));
+console.log(grad1.calcArray(1));
+console.log(grad1.calcArray(0.25));
+console.log(grad1.calcArray(0.5));
+console.log(grad1.calcArray(0.75));
